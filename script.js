42e4a9595c23f25c4ca2f662f460647aeb67062c
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-09-26 14:17:00 -0300

    Questions

---
+++ script.js
@@ -0,0 +1,42 @@
+const quizData = [
+  {
+      question: "Which language runs in a web browser?",
+      a: "Java",
+      b: "C",
+      c: "Python",
+      d: "JavaScript",
+      correct: "d",
+  },
+  {
+      question: "What does CSS stand for?",
+      a: "Central Style Sheets",
+      b: "Cascading Style Sheets",
+      c: "Cascading Simple Sheets",
+      d: "Cars SUVs Sailboats",
+      correct: "b",
+  },
+  {
+      question: "What does HTML stand for?",
+      a: "Hypertext Markup Language",
+      b: "Hypertext Markdown Language",
+      c: "Hyperloop Machine Language",
+      d: "Helicopters Terminals Motorboats Lamborginis",
+      correct: "a",
+  },
+  {
+      question: "What year was JavaScript launched?",
+      a: "1996",
+      b: "1995",
+      c: "1994",
+      d: "none of the above",
+      correct: "b",
+  },
+  {
+    question: "Who is the first man to take an step on the moon",
+    a: "Oscar Yopan",
+    b: "Neil Armstrong",
+    c: "Bill Gates",
+    d: "Christopher Lee",
+    correct: "b",
+},
+];
